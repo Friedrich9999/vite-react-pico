a7f1888037d3addd7e1ba878e13e155274b53042
PeterSteinerIII <126150305+Friedrich9999@users.noreply.github.com>
2025-06-24 09:19:28 +0200

    added heatmap testing

--- src/App.jsx
+++ src/App.jsx
@@ -7,73 +7,6 @@ import Fetcher from './fetcher';
 
 function App() {
 
-  const option = {
-    title: {
-      text: 'Stacked Line'
-    },
-    tooltip: {
-      trigger: 'axis'
-    },
-    legend: {
-      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
-    toolbox: {
-      feature: {
-        saveAsImage: {}
-      }
-    },
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        name: 'Email',
-        type: 'line',
-        stack: 'Total',
-        data: [120, 132, 101, 134, 90, 230, 210]
-      },
-      {
-        name: 'Union Ads',
-        type: 'line',
-        stack: 'Total',
-        data: [220, 182, 191, 234, 290, 330, 310]
-      },
-      {
-        name: 'Video Ads',
-        type: 'line',
-        stack: 'Total',
-        data: [150, 232, 201, 154, 190, 330, 410]
-      },
-      {
-        name: 'Direct',
-        type: 'line',
-        stack: 'Total',
-        data: [320, 332, 301, 334, 390, 330, 320]
-      },
-      {
-        name: 'Search Engine',
-        type: 'line',
-        stack: 'Total',
-        data: [820, 932, 901, 934, 1290, 1330, 1320]
-      }
-    ]
-  };
-
-  const {data} = useQuery({
-    queryKey: ['test'],
-    queryFn: getTodos
-  })
 
   const [graphtype, setGraphType] = useState(false)
 
@@ -104,7 +37,6 @@ function App() {
         </nav>
       </header>
       <main className='container'>
-        <Fetcher></Fetcher>
         <h1>Datentool</h1>
         <article>
           <h3>Einstellungen</h3>
@@ -155,9 +87,9 @@ function App() {
             </label>
           </div>
         </article>
-        <article>
+        <article height='200%'>
           <h3>Ausgabe</h3>
-          <ReactECharts option={option} height='800'/>
+          <Fetcher height="8000"></Fetcher>
         </article>
       </main>
       <footer className='container'>
@@ -165,11 +97,6 @@ function App() {
       </footer>
     </>
   )
-}
-
-const getTodos = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos")
-  return await response.json()
 }
 
 export default App
--- src/fetcher.jsx
+++ src/fetcher.jsx
@@ -1,23 +1,75 @@
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query';
+import ReactECharts from 'echarts-for-react';
 
 function Fetcher() {
 
-  const {data} = useQuery({
-    queryKey: ['test'],
+  const {data, isPending} = useQuery({
+    queryKey: ['test  '],
     queryFn: getTodos
   })
 
+  const option_heat = {
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: data ? data.xAxis : []
+    },
+    yAxis: {
+      type: 'category',
+      data: data ? data.yAxis : []
+    },
+    visualMap: {
+      min: data ? data.min : 0,
+      max: data ? data.max : 1,
+      calculable: true,
+      realtime: false,
+      inRange: {
+        color: [
+          '#313695',
+          '#4575b4',
+          '#74add1',
+          '#abd9e9',
+          '#e0f3f8',
+          '#ffffbf',
+          '#fee090',
+          '#fdae61',
+          '#f46d43',
+          '#d73027',
+          '#a50026'
+        ]
+      }
+    },
+    series: [
+      {
+        name: 'Gaussian',
+        type: 'heatmap',
+        data: data ? data.data : [],
+        emphasis: {
+          itemStyle: {
+            borderColor: '#333',
+            borderWidth: 1
+          }
+        },
+        progressive: 1000,
+        animation: true
+      }
+    ]
+  };
+
   return (
     <>
-        <h1>{JSON.stringify(data?.slice(0,10))}</h1>
+      {isPending ? <div aria-busy="true"></div> : <ReactECharts option={option_heat}/>}
     </>
   )
 }
 
 const getTodos = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos")
-  return await response.json()
+  console.log("trying to access server")
+  const response = await fetch("http://localhost:8000/heatmap/2023-01-01/2025-01-01")
+  const dat = await response.json()
+  console.log(dat)
+  return dat
 }
 
 export default Fetcher
